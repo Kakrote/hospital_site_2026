9ce6c39fbf640878191d2d88d956dd1e482b885e
Kakrote <anshul.pundir111@gmail.com>
2026-05-05 17:54:42 +0530

    added changes in the nextconfig files

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,15 +1,16 @@
 "use client";
 
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+// import Lenis from "@studio-freight/lenis";
+import Lenis from 'lenis';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         const lenis = new Lenis({
             duration: 1.5, // smoothness
             smoothWheel: true,
-            smoothTouch: false,
             wheelMultiplier: 1,
+            touchMultiplier: 2, // adjust speed/sensitivity
         });
 
         function raf(time: any) {
